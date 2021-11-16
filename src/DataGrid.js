b96188bc040f52b8f2155659249666f93a8d57d2
Edris Berg <ali_berg1@yahoo.com>
2021-11-16 09:18:53 -0800

    files setup

---
+++ src/DataGrid.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DataGrid extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
