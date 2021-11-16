b9699e7a998127bd434f903cfa1c2030880b09bd
Ben Mills <fuzzyllama@gmail.com>
2021-11-16 11:34:22 -0800

    Renders dummy data to main route

---
+++ src/ProductCard.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default class ProductCard extends Component {
+  render() {
+    return (
+      <>
+        <Card sx={{maxWidth: 345}}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={this.props.image}
+            alt={this.props.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {this.props.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {this.props.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Add to Cart</Button>
+          </CardActions>
+        </Card>
+      </>
+    )
+  }
+}
+
--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+//import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+//import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+//const Item = styled(Paper)(({ theme }) => ({
+//...theme.typography.body2,
+//padding: theme.spacing(1),
+//textAlign: 'center',
+//color: theme.palette.text.secondary,
+//}));
+
+
+import React, {Component} from 'react';
+import ProductCard from './ProductCard';
 
 export default class Products extends Component {
-    render() {
-        return (
-            <div>
+  render() {
 
-            </div>
-        )
-    }
+    // filtering happens here
+    let filteredProducts = [];
+    filteredProducts = [...this.props.allProducts]
+
+    return (
+      <div>
+        <Box sx={{width: '100%'}}>
+          <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
+            {
+              filteredProducts.map((element, index) => {
+                return (
+                  <Grid key={index} item xs={6}>
+                    <ProductCard
+                      name={element.name}
+                      image={element.image}
+                      description={element.description}
+                    />
+                  </Grid>
+                );
+              })
+            }
+          </Grid>
+        </Box>
+      </div>
+    )
+  }
 }
