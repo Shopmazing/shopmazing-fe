26e5495d6c513bae82530fcc2b4e94a115987132
rushabhjsoni <slayer.r1@gmail.com>
2021-11-18 01:10:04 +0530

    created product modal for main page

--- src/home/ProductModal.js
+++ src/home/ProductModal.js
@@ -26,20 +26,3 @@ class ProductModal extends Component {
 export default ProductModal;
 
 
-
-{/* <Modal
-        show={this.props.openModal}
-        onHide={this.props.closeModal}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">{this.props.products.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body><img src={this.props.products.image} alt=""/><p>
-        {this.props.products.description}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.props.closeModal}>Close</Button>
-        </Modal.Footer>
-      </Modal> */}
