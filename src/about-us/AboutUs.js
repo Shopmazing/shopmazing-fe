b1998575df0627fe508b5ff1cd05186eb2e402f3
Edris Berg <ali_berg1@yahoo.com>
2021-11-17 10:50:42 -0800

    about us page

--- src/about-us/AboutUs.js
+++ src/about-us/AboutUs.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react'
 export default class AboutUs extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="mainDiv">
+                <p> Team Members </p>
+                <div>
+                    <img src=""></img>
+                    <p>Ben Mills</p>
+                </div>
+                <div>
+                    <img src=""></img>
+                    <p>Kirk Garrison</p>
+                </div>
+                <div>
+                    <img src=""></img>
+                    <p>Edris Berg</p>
+                </div>
+                <div>
+                    <img src=""></img>
+                    <p>Jordan Fleming</p>
+                </div>
+                <div>
+                    <img src=""></img>
+                    <p>Rushabh Soni</p>
+                </div>
             </div>
         )
     }
