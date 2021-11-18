6543c4dd8a1a4606fcb731d440384f57610a3c05
Edris Berg <ali_berg1@yahoo.com>
2021-11-18 10:22:43 -0800

    about-us flex

--- src/about-us/AboutUs.js
+++ src/about-us/AboutUs.js
@@ -4,37 +4,49 @@ import kirk from './images/kirk.jpg';
 import soni from './images/soni.jpg';
 import ed from './images/Ed.png';
 import jordan from './images/jordan.jpg';
-import './aboutus.css'
+import './about-us.css'
 
 export default class AboutUs extends Component {
     render() {
         return (
-            <div className="mainDiv">
-                <h1> Shopmazing team members </h1>
-                <div id="benDiv">
-                    <h2>Ben Mills</h2>
-                    <img src={ben} alt="Ben" />
-                    <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
-                </div>
-                <div id="kirkDiv">
-                    <h2>Kirk Garrison</h2>
-                    <img src={kirk} alt="Kirk" />
-                    <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
-                </div>
-                <div id="edDiv">
-                    <h2>Edris Berg</h2>
-                    <img src={ed} alt="Edris" />
-                    <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
-                </div>
-                <div id="jordanDiv">
-                    <h2>Jordan Fleming</h2>
-                    <img src={jordan} alt="Jordan" />
-                    <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
-                </div>
-                <div id="soniDiv">
-                    <h2>Rushabh Soni</h2>
-                    <img src={soni} alt="Soni" />
-                    <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+            <div className="about-us">
+                <h1 className="about-us__heading"> Shopmazing team </h1>
+                <div className="about-us__cards">
+                    <div className="about-us__cards-card">
+                        <img src={ben} alt="" />
+                        <div className="about-us__cards-card-text">
+                            <h2>Ben Mills</h2>
+                            <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                        </div>
+                    </div>
+                    <div className="about-us__cards-card">
+                        <img src={kirk} alt="" />
+                        <div className="about-us__cards-card-text">
+                            <h2>Kirk Garrison</h2>
+                            <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                        </div>
+                    </div>
+                    <div className="about-us__cards-card">
+                        <img src={ed} alt="" />
+                        <div className="about-us__cards-card-text">
+                            <h2>Edris Berg</h2>
+                            <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                        </div>
+                    </div>
+                    <div className="about-us__cards-card">
+                        <img src={jordan} alt="" />
+                        <div className="about-us__cards-card-text">
+                            <h2>Jordan Fleming</h2>
+                            <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                        </div>
+                    </div>
+                    <div className="about-us__cards-card">
+                        <img src={soni} alt="" />
+                        <div className="about-us__cards-card-text">
+                            <h2>Rushabh Soni</h2>
+                            <p>mply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
