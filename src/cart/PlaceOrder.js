e4b581067d311944fca31f80e6acd9c173f8332b
Ben Mills <fuzzyllama@gmail.com>
2021-11-19 08:33:50 -0800

    Adds snackbars for successful cart sumissions and out of ranges

--- src/cart/PlaceOrder.js
+++ src/cart/PlaceOrder.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
+import Success from './Success';
 
 export default class PlaceOrder extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: false,
+    }
+  }
 
   handleClick = (e) => {
     e.preventDefault();
     this.props.placeOrder(this.props.cart);
+    this.props.setTotal(0);
+    this.props.setSuccess();
   }
 
   render() {
     return (
-      <Button variant="secondary" onClick={this.handleClick}>Place Order</Button>
+      <>
+        <Button variant="primary" onClick={this.handleClick}>Place Order</Button>
+      </>
     )
   }
 }
